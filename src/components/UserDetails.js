289cff82e22420f42453e5c7e4e833eeb765e729
kuldeepparmar <kuldeep07122002@gmail.com>
2025-07-14 23:49:08 +0530

    change via ai

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,31 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UserDetails = () => {
-  const {id} = useParams();
-  console.log("waht is come in id",id);
-    const [item,setItem]=useState([]);
-    useEffect(()=>{
-     fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then((res)=>res.json()).then(data=>(
-        setItem(data)
-     ))
-    },[])
-    console.log("what is come in item",item);
+  const { id } = useParams();
+  const [item, setItem] = useState(null); // Change to null
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => setItem(data))
+      .catch((error) => console.error('Error fetching user data:', error));
+  }, [id]);
+
   return (
     <div>
-        {
-            item ? (
-                <div>
-                    <p>Name:{item.name}</p>
-                    <p>Username:{item.username}</p>
-                    <p>Email:{item.email}</p>
-                    <p>Phone:{item.phone}</p>
-                    <p>Website:{item.website}</p>
-                </div>
-            ):(<p>Loading...</p>)
-        }
+      {item ? (
+        <div>
+          <p>Name: {item.name}</p>
+          <p>Username: {item.username}</p>
+          <p>Email: {item.email}</p>
+          <p>Phone: {item.phone}</p>
+          <p>Website: {item.website}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default UserDetails
+export default UserDetails;
