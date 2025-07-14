f0b253210732ed2d767b4d83d8014a0f558f07c1
kuldeepparmar <kuldeep07122002@gmail.com>
2025-07-14 23:22:40 +0530

    Done

---
+++ src/components/UserDetails.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+const UserDetails = () => {
+  const {id} = useParams();
+  console.log("waht is come in id",id);
+    const [item,setItem]=useState([]);
+    useEffect(()=>{
+     fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then((res)=>res.json()).then(data=>(
+        setItem(data)
+     ))
+    },[])
+    console.log("what is come in item",item);
+  return (
+    <div>
+        {
+            item && (
+                <div>
+                    <p>Name:{item.name}</p>
+                    <p>Username:{item.username}</p>
+                    <p>Email:{item.email}</p>
+                    <p>Phone:{item.phone}</p>
+                    <p>Website:{item.website}</p>
+                </div>
+            )
+        }
+    </div>
+  )
+}
+
+export default UserDetails
---
+++ src/components/UserList.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom';
+const UserList = () => {
+    const [data,setData] =useState([]);
+     useEffect(()=>{
+      fetch("https://jsonplaceholder.typicode.com/users").then(res=>res.json()).then(data=>(
+        setData(data)
+      ))
+    },[]);
+    console.log("what is come in data",data);
+  return (
+    <div> 
+       <h1>User List</h1>
+       {
+        data && data.map((item,index)=>(
+            <li key={index}><NavLink to={`/users/${item.id}`}>{item.name}</NavLink></li>
+        ))
+       }
+    </div>
+  )
+}
+
+export default UserList
